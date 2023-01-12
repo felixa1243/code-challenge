f918a7e19c56411efc2afec8f1b6e95089448787
Rajiph iqbal <felixarajiph@gmail.com>
2023-01-12 20:49:55 +0700

    init commit

---
+++ 1.py
@@ -0,0 +1,17 @@
+ for i in range(1, 4):
+    for j in range(1, 6):
+        print(j, end=" ")
+    print()
+    for k in range(5, 0, -1):
+        print(k, end=" ")
+    print()
+#output
+#1 2 3 4 5
+#5 4 3 2 1
+#1 2 3 4 5
+#5 4 3 2 1
+#1 2 3 4 5
+#5 4 3 2 1
+#It will print numbers 1-5 in four rows, and then print numbers 5-1 in four rows. Each inner loop iteration will print a number, and if the inner loop completed the outer loop will print new line before printing numbers 5-1 in descending order, and again a new line after that.
+
+#The outer for loop iterates 3 times, on each iteration the inner for loop iterates 5 times and prints numbers 1-5, then the second inner for loop iterates 5 times, but in reverse order and prints the numbers 5-1 and goes to the next outer loop iteratio
---
+++ 2.py
@@ -0,0 +1,17 @@
+ for i in range(1, 6):
+    for j in range(1, i+1):
+        print("*", end="")
+    print()
+#This code uses nested for loops to generate a right triangle with 5 rows. The outer loop uses the range() function to iterate 5 times, and the inner loop uses the range() function to iterate i times.
+
+#On each iteration of the outer loop, the inner loop is executed and the * character is printed i times, where i is the current value of the outer loop. The end="" argument is used to prevent the cursor from moving to the next line after each print statement, this way all the * characters are printed on the same line. The print() statement at the end of the inner loop causes the cursor to move to the next line after the inner loop completes.
+
+#So, the output will be a right triangle shape with 5 rows and the number of * on each row is increasing from 1 to 5, like this:
+
+#Copy code
+#*
+#**
+#***
+#****
+#*****
+#This is a common pattern used to create a triangle shape with asterisks or any other characters.
